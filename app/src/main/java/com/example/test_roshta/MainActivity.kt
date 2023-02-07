4e2a12fa48fd9266661f2f9e093d897054594fad
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-07 21:00:17 +0200

    Finish the page xml

--- app/src/main/java/com/example/test_roshta/MainActivity.kt
+++ app/src/main/java/com/example/test_roshta/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.test_roshta
 
-import android.content.Context
+
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test_roshta.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
     }
 
 }
