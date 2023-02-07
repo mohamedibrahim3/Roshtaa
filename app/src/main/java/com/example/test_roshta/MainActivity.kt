d3fbd8bf611c583c9dc692c2b09e796b2633d2e8
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-07 22:38:21 +0200

    add bottom sheet

--- app/src/main/java/com/example/test_roshta/MainActivity.kt
+++ app/src/main/java/com/example/test_roshta/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.test_roshta
 
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test_roshta.databinding.ActivityMainBinding
+import com.example.test_roshta.databinding.BottomsheetlayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.addRoshtaPhoto?.setOnClickListener {
+            val view: View = layoutInflater.inflate(R.layout.bottomsheetlayout,null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
         setContentView(binding?.root)
     }
 
